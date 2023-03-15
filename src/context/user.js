63e7dd2db7a6e8b6eb64c782c16bcd65a6c92210
Jahdon Ervin <Jahdonervin@gmail.com>
2023-03-14 20:40:01 -0400

    commit tree a27a3ce0263fc4fa0e811487f4d94b2417e677c6

---
+++ src/context/user.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const UserContext = React.createContext();
+
+function UserProvider({ children }) {
+    const [user, setUser] = useState(null);
+    // const currentUser = {
+    //     name: "Duane",
+    //     interests: ["Coding", "Biking", "Cooking"],
+    //   };
+      return (
+        <UserContext.Provider value={null}>
+            {children}
+        </UserContext.Provider>
+    );
+ }
+
+export { UserContext, UserProvider };
